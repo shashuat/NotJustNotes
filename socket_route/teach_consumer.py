8bbd0a41b4f62543b3acf15951d5ee7029ec8bc6
rhythmrx9 <rhythmrx9@gmail.com>
2022-11-21 23:56:51 +0530

    feat: live transcription live

--- socket_route/teach_consumer.py
+++ socket_route/teach_consumer.py
@@ -18,21 +18,39 @@ class TeachConsumer(WebsocketConsumer):
         self.head_size = 256
 
         self.transcript = ""
+        self.summary = ""
         self.chunks = []
 
         async_to_sync(self.channel_layer.group_add)(f"class-{self.class_id}-teacher", self.channel_name)
         async_to_sync(self.channel_layer.group_add)(f"class-{self.class_id}-joined", self.channel_name)
 
         data = course.objects.get(course_id=self.class_id)
+        self.class_name = data.course_name
 
         async_to_sync(self.channel_layer.group_send)(
             f"class-{self.class_id}-student",
             {
                 "type": "class_started",
-                "class_id": f"{self.class_id}",
-                "course_name": f"{data.course_name}"
+                "class_id": self.class_id,
+                "course_name": data.course_name
             },
         )
+
+        set_con = {
+                "teach_class": self.class_name,
+                "teach_time": "00:00:00",
+                "teach_er" : f"{self.user}"
+                }
+
+        async_to_sync(self.channel_layer.group_send)(
+                f"class-{self.class_id}-teacher",
+                { 
+                    "type": "disp",
+                    "contents": set_con
+                }
+         )
+
+
 
         self.model = whisper.load_model("tiny")
 
@@ -74,21 +92,39 @@ class TeachConsumer(WebsocketConsumer):
                 f"class-{self.class_id}-joined",
                 {
                     "type": "transcription",
-                    "trans": f"{script}"
+                    "trans": script ,
                     },)
 
     def send_script(self, event):
         async_to_sync(self.channel_layer.send)(f"{event['sender']}",
             {
                 "type" : "beg_script",
-                "text" : f"{self.transcription}"
+                "trans" : self.transcript,
+                "summary" : self.summary,
+                "teach_er" : f"{self.user}",
+                "teach_class": self.class_name,
+                "teach_time" : "00:00:00"
+                
                 })
 
 
     def transcription(self, event):
         script = event["trans"]
 
-        self.send(text_data=json.dumps(
-            {
-                "trans" : f"{script}"
-            }))
+        contents = {
+                "d_type": "lyrics_para",
+                "d_text": script}
+
+
+        self.send(text_data = json.dumps(contents))
+
+    def disp(self, event):
+        contents = event["contents"]
+        
+        for k,v in contents.items():
+            self.send(text_data = json.dumps(
+                {
+                    "d_type" : k,
+                    "d_text" : v,
+                    }))
+
